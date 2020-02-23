77a1894ed439f119b2ac3e83afccc428372c8350
Tran Tien Manh <manhtt102@gmail.com>
2020-02-23 23:28:06 +0700

    fix item.py in model

--- models/item.py
+++ models/item.py
@@ -12,8 +12,7 @@ class ItemModel(db.Model):
 
     def __init__(self, name, price, store_id):
         self.name = name
-        self.
-        price = price
+        self.price = price
         self.store_id = store_id
 
     def json(self):
